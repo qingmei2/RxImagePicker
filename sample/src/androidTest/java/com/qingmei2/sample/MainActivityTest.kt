9eb7542a3c2fe6661f7b80c31ea8eedbe6cb2da3
qingmei2 <mq2553299@qq.com>
2018-07-25 17:41:12 +0800

    Add the sample test code.

---
+++ sample/src/androidTest/java/com/qingmei2/sample/MainActivityTest.kt
@@ -0,0 +1,66 @@
+package com.qingmei2.sample
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import android.support.test.espresso.intent.matcher.IntentMatchers.toPackage
+import android.support.test.espresso.intent.rule.IntentsTestRule
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.filters.LargeTest
+import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    val TEST_PACKAGE_NAME = "com.qingmei2.sample"
+
+    @Rule
+    @JvmField
+    var tasksActivityTestRule = IntentsTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun testJump2SystemActivity() {
+        checkScreenJumpEvent({ R.id.btn_system_picker },
+                { ".system.SystemActivity" })
+    }
+
+    @Test
+    fun testJump2ZhihuActivity() {
+        checkScreenJumpEvent({ R.id.btn_zhihu_picker },
+                { ".zhihu.ZhihuActivity" })
+    }
+
+    @Test
+    fun testJump2WeChatActivity() {
+
+        checkScreenJumpEvent({ R.id.btn_wechat_picker },
+                { ".wechat.WechatActivity" })
+    }
+
+    fun checkScreenJumpEvent(buttonId: Supplier<Int>,
+                             shortName: Supplier<String>,
+                             packageName: Supplier<String> = { TEST_PACKAGE_NAME }) {
+
+        onView(withId(buttonId())).perform(click()).check(doesNotExist())
+
+        intended(allOf(
+                toPackage(packageName()),
+                hasComponent(hasShortClassName(shortName()))))
+
+        pressBack()
+        onView(withId(buttonId())).check(matches(isDisplayed()))
+    }
+}
+
+typealias Supplier<T> = () -> T
